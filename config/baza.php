74af84939c6b3a0c3f17385fd17ac71cf5f634db
Ecchi <Mugiwar@users.noreply.github.com>
2016-10-10 14:32:37 +0300

    Update baza.php

--- config/baza.php
+++ config/baza.php
@@ -2,9 +2,9 @@
 return array(
 	'db' => array(
 		'driver' 		 => 'Pdo',
-		'dsn' 	 		 => 'mysql:dbname=waterhne_udhtu;hostname=127.0.0.1',
-		'username' => 'udhtu',
-		'password' => 'Zai6phah',
+		'dsn' 	 		 => 'mysql:dbname=;hostname=127.0.0.1',
+		'username' => '',
+		'password' => '',
 		'driver_options' => array(
 			PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
 		),
